b86a40ccd881f1180b96dd47dc668c57f1bfbd4e
Yoshiya Hinosawa <stibium121@gmail.com>
2016-04-17 17:28:49 +0900

    Pipe vinyl directly to response object

--- test.js
+++ test.js
@@ -5,3 +5,5 @@ require('vinyl-fs').src('fixture/**/*.js')
         file.contents = require('browserify')(file.path).bundle()
         this.queue(file)
     })).pipe(require('./')())
+
+require('vinyl-fs').src('fixture/**/*.png').pipe(require('./')())
